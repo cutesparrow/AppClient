908b45c8ba82c6d46921e901a0139b8ff3722378
ziven <shigaoyu615@gmail.com>
2020-12-18 11:13:27 +1100

    Initial Commit

---
+++ AppClient/AppClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppClientApp.swift
+//  AppClient
+//
+//  Created by gaoyu shi on 18/12/20.
+//
+
+import SwiftUI
+
+@main
+struct AppClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
